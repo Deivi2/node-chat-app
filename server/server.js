09340f332f04d85aa489846ac15b3ff30fba44af
Deividas <Puzeras@gmail.com>
2016-12-16 19:09:43 +0000

    adding chat app

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicpath = path.join(__dirname, '../public');
+const port = process.evc.PORT || 3000;
+const app = express();
+
+//middleware that starts index.html file
+app.use(express.static(publicpath));
+
+
+
+app.listen(post,() => {
+ console.log('Running server on port', port)
+});
